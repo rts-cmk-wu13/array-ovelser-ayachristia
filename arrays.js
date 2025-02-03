4fc7c4e2293b398e6d7d14e771757f43113ae769
iamayac <ayachristia@gmail.com>
2025-02-03 23:09:44 +0100

    opgave 1 løst

--- arrays.js
+++ arrays.js
@@ -1,9 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
-    let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
-    let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
-    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
+document.addEventListener("DOMContentLoaded", function () {
+  let catsArray = [
+    "Ollie",
+    "Sophie",
+    "Salem",
+    "Tiger",
+    "Binx",
+    "Pumpkin",
+    "Penny",
+    "Lenny",
+  ];
+  let dogsArray = [
+    "Barney",
+    "Molly",
+    "Baxter",
+    "Polly",
+    "Buddy",
+    "Suki",
+    "Watson",
+  ];
+  let fruitsArray = [
+    "Banana",
+    "Orange",
+    "Pineapple",
+    "Mango",
+    "Plum",
+    "Apricot",
+  ];
+  let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21];
 
-    // --- Løs opgaverne herunder --- 
+  // --- Løs opgaverne herunder ---
 
-}) // ends DOMContentLoaded
+  //   -----opgave 1
+
+  let newNumbers = numbersArray.filter((number) => number > 20);
+  console.log(newNumbers);
+}); // ends DOMContentLoaded
